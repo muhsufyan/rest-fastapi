af0a2e2a4c4c8546c8dd5046802794608dbae56e
muhsufyan <muh.sufyan.ts@gmail.com>
2022-03-15 16:16:12 +0700

    cors

--- app/main.py
+++ app/main.py
@@ -2,13 +2,26 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 # from .config import setting
 
 # ini dinon-aktifkan karena kita menggunakan alembic
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Dokumentasi untuk api")
-
+# url yg diinzinkan
+origins = [
+    "https://www.google.com/",
+    "http://localhost/"
+]
+# cors bawaan fastapi
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # panggil semua route post.py
 app.include_router(post.router)
 # panggil semua route user.py
